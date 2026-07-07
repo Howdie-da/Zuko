6766e9f8e89d1bb6c2f622c527c73caa7c3cede3
Akshat Nagar <akshatn9414@gmail.com>
2026-07-07 22:00:11 +0530

    fix .env import in app.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,7 @@
-import dotenv from "dotenv";
+import 'dotenv/config';
 import { app } from './app.js'
 import { connectDB } from "./db/index.js";
 
-dotenv.config({
-    path: '.env'
-})
 
 connectDB()
 .then(() => {
