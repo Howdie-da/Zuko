a2f9f53ab4b80b5ca3f49a70e7783f096d87dc53
Akshat Nagar <akshatn9414@gmail.com>
2026-07-08 21:41:13 +0530

    cors headers fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,30 +1,30 @@
 import express from "express";
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-const app = express()
+const app = express();
 
 app.use(cors({
     origin: [
-        `${process.env.CORS_ORIGIN}`, 
+        process.env.CORS_ORIGIN,
         'capacitor://localhost', 
         'http://localhost'
     ],
-    credentials: true
-}))
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
-app.use(express.json({limit: "16kb"}))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(cookieParser());
 
-
+// 3. Routes
 import { authRouter } from "./routes/auth.routes.js";
 import { animeRouter } from "./routes/anime.routes.js";
 import { userRouter } from "./routes/user.routes.js";
 
-app.use("/api/auth", authRouter)
-app.use("/api/anime", animeRouter)
-app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/anime", animeRouter);
+app.use("/api/user", userRouter);
 
-export {
-    app
-} 
+export { app };
