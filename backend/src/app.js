d7c85fc30bc8be0155aa3733aa67c4f5d7179b43
Akshat Nagar <akshatn9414@gmail.com>
2026-07-08 21:35:00 +0530

    updated Cors for apk

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,11 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(cors({
-    origin: `${process.env.CORS_ORIGIN}`,
+    origin: [
+        `${process.env.CORS_ORIGIN}`, 
+        'capacitor://localhost', 
+        'http://localhost'
+    ],
     credentials: true
 }))
 
